179b08ef12fdf3ad2df029a6df019952c4c1dd26
Peter Vera <petercvera@gmail.com>
2026-05-02 15:56:49 -0700

    add wireit example

---
+++ wireit/packages/sample/src/sample-input.js
@@ -0,0 +1,4 @@
+// This comment will be strippedd from the output, modifying it should not trigger a rebuild because of the stripped comments output being the same.
+export default function sampleInput() {
+  return "sampleInput";
+}
