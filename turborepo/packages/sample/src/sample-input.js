e3b2956af409aa5320a8e58641bab887417b4375
Peter Vera <petercvera@gmail.com>
2026-05-02 14:59:42 -0700

    build-only dependency

--- turborepo/packages/sample/src/sample-input.js
+++ turborepo/packages/sample/src/sample-input.js
@@ -1,5 +1,4 @@
 // This comment will be stripped from the output, modifying it should not trigger a rebuild because of the stripped comments output being the same.
-// testing asdf
 export default function sampleInput() {
   return "sampleInput";
 }
